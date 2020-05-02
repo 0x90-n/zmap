f4c44495a1b6f72c617b3a7e303c6b8ca9a511f3
ewust <ewust@zbuff-research-scan.colorado.edu>
2020-05-02 14:19:30 -0600

    traceroute needs to not filter duplicates (and hitrate needs to obey that)

--- src/monitor.c
+++ src/monitor.c
@@ -162,7 +162,7 @@ static void export_stats(int_status_t *intrnl, export_status_t *exp,
 	uint32_t total_tried_sent = iterator_get_tried_sent(it);
 	uint32_t total_fail = iterator_get_fail(it);
 	uint32_t total_recv = zrecv.pcap_recv;
-	uint32_t recv_success = zrecv.success_unique;
+	uint32_t recv_success = zrecv.success_total;
 	uint32_t app_success = zrecv.app_success_unique;
 	double cur_time = now();
 	double age = cur_time - zsend.start; // time of entire scan
